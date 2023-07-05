a528b65163c40ec574238f996294fb8908e8a1f7
成龍 長尾 <210215@jc-21.jp>
2023-07-05 17:15:01 +0900

    メッシュシステム追加

---
+++ GameEngine/MeshSystem.h
@@ -0,0 +1,9 @@
+#pragma once
+#include"System.h"
+#include"Coordinator.h"
+#include"Engine/DirectX_11/Fbx.h"
+class MeshSystem : public System
+{
+	//Fbx* GetMesh
+	
+};
--- GameEngine/Test_Model_ECSver.h
+++ GameEngine/Test_Model_ECSver.h
@@ -1,9 +1,13 @@
 #pragma once
-
+#include"Material.h"
+#include<vector>
+#include"Coordinator.h"
 //メッシュクラスとマテリアルクラスをコンポーネントとして置く
 //モデルクラスで描画
 class Test_Model_ECSver
 {
+	Entity meshEntity_;
+	std::vector<Material> materialList_;
 
 };
 
