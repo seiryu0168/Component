c371b4c91ad29c7bd82716d06ebf022f71c959c2
翔 玉河 <210306@jc-21.jp>
2023-08-10 17:46:20 +0900

    Revert "細かい調整が一番難しい"
    This reverts commit 6615b3d68b6c815902de513a08a31de5c53b594a.

--- GameEngine/SelectUI.cpp
+++ GameEngine/SelectUI.cpp
@@ -9,23 +9,25 @@
 #include "ImGui/imgui.h"
 namespace
 {
-	const static float	MOVE = 0.35f;
+	const static float	MOVE = -500.0f;
 	const static int	MAX_MOVE_TIME = 11;
-	const static XMFLOAT2  IMAGE_OFFSET = { 0.55f,-0.49f };
-	const static float  IMAGE_RATIO = -10.0f;
+	//const static XMINT2 MAX_SELECT = { 0,4 };
+	const static XMFLOAT2  IMAGE_OFFSET = { 900.0f,600.0f };
+	const static float  IMAGE_RATIO = 5.0f;
 	const static int	TEXT_SIZE_DEFAULT = 54;
 	const static int	TEXT_SIZE_PICKUP = 90;
 
 	//それぞれのテキストの位置
 	const std::map<int, XMFLOAT2> Position = {
-		{-2, {0.15f, -0.2f}},
-		{-1, {0.15f, 0.15f}},
-		{ 0, {0.15f, 0.5f}},
-		{ 1, {0.15f, 0.85f}},
-		{ 2, {0.15f, 1.2f}},
+		{-2, {500, 400}},
+		{-1, {500, -100}},
+		{ 0, {500, -600}},
+		{1, {500, -1100}},
+		{2, {500, -1600}},
+		{3, {500, -2100}}
 	};
 
-	const static XMFLOAT3 POSITION_DEFAULT = { -9999,-9999,0 };
+	const static XMFLOAT3 POSITION_DEFAULT = { -9999,0,0 };
 }
 
 SelectUI::SelectUI(Object* parent)
@@ -51,22 +53,41 @@ void SelectUI::Initialize()
 	//テキスト
 	{
 		Text text1("Game1", "りいてがき筆", { 0,0,500,50 });
+		/*XMFLOAT3 pos = { 500,-600.0f,0 };
+		basePosList_.push_back(pos);*/
+		//text1.SetPosition({ pos.x, pos.y });
+		//text1.SetAlinmentType(LEFT_CENTER);
 		moveUIList_.push_back((int)AddComponent<Text>(text1));
 		
 	}
 	//テキスト2
 	{
 		Text text2("Game2", "りいてがき筆", { 0,0,500,50 });
+		//text2.SetText("Stage02");
+		/*XMFLOAT3 pos = { 500.0f,-1100.0f,0 };
+		basePosList_.push_back(pos);*/
+		//text2.SetPosition({ pos.x, pos.y });
+		//text2.SetAlinmentType(LEFT_CENTER);
+		//text2.SetFontStyle(DWRITE_FONT_STYLE_ITALIC, 0, 3);
+		//text2.SetFontWeight(DWRITE_FONT_WEIGHT_BOLD, 0, 5);
 		moveUIList_.push_back((int)AddComponent<Text>(text2));
 	}
 	//テキスト3
 	{
 		Text text3("Game3", "りいてがき筆", { 0,0,500,50 });
+		//text3.SetText("Stage03");
+		/*XMFLOAT3 pos = { 500.0f,-1600.0f,0 };
+		basePosList_.push_back(pos);*/
+		//text3.SetPosition({ pos.x, pos.y });
 		moveUIList_.push_back((int)AddComponent<Text>(text3));
 	}
 	//テキスト2
 	{
 		Text text2("Game4", "りいてがき筆", { 0,0,500,50 });
+		//text2.SetText("Stage04");
+		/*XMFLOAT3 pos = { 500.0f,-1100.0f,0 };
+		basePosList_.push_back(pos);*/
+		//text2.SetPosition({ pos.x, pos.y });
 		text2.SetAlinmentType(LEFT_CENTER);
 		text2.SetFontStyle(DWRITE_FONT_STYLE_ITALIC, 0, 3);
 		text2.SetFontWeight(DWRITE_FONT_WEIGHT_BOLD, 0, 5);
@@ -75,17 +96,28 @@ void SelectUI::Initialize()
 	//テキスト3
 	{
 		Text text3("Game5", "りいてがき筆", { 0,0,500,50 });
+		//text3.SetText("Stage05");
+		/*XMFLOAT3 pos = { 500.0f,-1600.0f,0 };
+		basePosList_.push_back(pos);*/
+		//text3.SetPosition({ pos.x, pos.y });
 		moveUIList_.push_back((int)AddComponent<Text>(text3));
 	}
 	//テキスト3
 	{
 		Text text3("Game6", "りいてがき筆", { 0,0,500,50 });
 		text3.SetText("Stage06");
+		/*XMFLOAT3 pos = { 500.0f,-1600.0f,0 };
+		basePosList_.push_back(pos);*/
+		//text3.SetPosition({ pos.x, pos.y });
 		moveUIList_.push_back((int)AddComponent<Text>(text3));
 	}
 	//テキスト4
 	{
 		Text text4("Game7", "りいてがき筆", { 0,0,500,50 });
+		//text3.SetText("Stage06");
+		/*XMFLOAT3 pos = { 500.0f,-1600.0f,0 };
+		basePosList_.push_back(pos);*/
+		//text3.SetPosition({ pos.x, pos.y });
 		moveUIList_.push_back((int)AddComponent<Text>(text4));
 	}
 
@@ -102,6 +134,10 @@ void SelectUI::Initialize()
 	{
 		Image image1;
 		image1.Load("Assets\\Image\\Image_Stage1.png");
+
+		/*image1.SetPositionAtPixel({ -basePosList_[0].x + IMAGE_OFFSET.x,
+									(basePosList_[0].y +IMAGE_OFFSET.y) * IMAGE_RATIO,
+									basePosList_[0].z });*/
 		AddComponent<Image>(image1);
 	}
 	//画像2
@@ -109,6 +145,10 @@ void SelectUI::Initialize()
 
 		Image image2;
 		image2.Load("Assets\\Image\\Image_Stage2.png");
+		
+		/*image2.SetPositionAtPixel({ -basePosList_[1].x + IMAGE_OFFSET.x,
+									(basePosList_[1].y+IMAGE_OFFSET.y)* IMAGE_RATIO,
+								    basePosList_[1].z });*/
 		AddComponent<Image>(image2);
 	}
 	//画像3
@@ -116,6 +156,10 @@ void SelectUI::Initialize()
 
 		Image image3;
 		image3.Load("Assets\\Image\\Image_Stage3.png");
+		
+		/*image3.SetPositionAtPixel({ -basePosList_[2].x + IMAGE_OFFSET.x,
+									(basePosList_[2].y+IMAGE_OFFSET.y)* IMAGE_RATIO,
+									basePosList_[2].z });*/
 		AddComponent<Image>(image3);
 	}
 	//画像2
@@ -123,6 +167,10 @@ void SelectUI::Initialize()
 
 		Image image2;
 		image2.Load("Assets\\Image\\Parrot.jpg");
+
+		/*image2.SetPositionAtPixel({ -basePosList_[1].x + IMAGE_OFFSET.x,
+									(basePosList_[1].y + IMAGE_OFFSET.y) * IMAGE_RATIO,
+									basePosList_[1].z });*/
 		AddComponent<Image>(image2);
 	}
 	//画像3
@@ -130,6 +178,10 @@ void SelectUI::Initialize()
 
 		Image image3;
 		image3.Load("Assets\\Image\\Parrot.jpg");
+
+		/*image3.SetPositionAtPixel({ -basePosList_[2].x + IMAGE_OFFSET.x,
+									(basePosList_[2].y + IMAGE_OFFSET.y) * IMAGE_RATIO,
+									basePosList_[2].z });*/
 		AddComponent<Image>(image3);
 	}
 	//画像3
@@ -137,6 +189,10 @@ void SelectUI::Initialize()
 
 		Image image3;
 		image3.Load("Assets\\Image\\Parrot.jpg");
+
+		/*image3.SetPositionAtPixel({ -basePosList_[2].x + IMAGE_OFFSET.x,
+									(basePosList_[2].y + IMAGE_OFFSET.y) * IMAGE_RATIO,
+									basePosList_[2].z });*/
 		AddComponent<Image>(image3);
 	}
 	//画像3
@@ -144,12 +200,16 @@ void SelectUI::Initialize()
 
 		Image image3;
 		image3.Load("Assets\\Image\\Parrot.jpg");
+
+		/*image3.SetPositionAtPixel({ -basePosList_[2].x + IMAGE_OFFSET.x,
+									(basePosList_[2].y + IMAGE_OFFSET.y) * IMAGE_RATIO,
+									basePosList_[2].z });*/
 		AddComponent<Image>(image3);
 	}
 	
 	for (int i = 0; i < moveUIList_.size(); i++)
 	{
-		GetComponent<Text>(i).SetRatio(Position.at(2).x, Position.at(2).y);
+		GetComponent<Text>(i).SetPosition(Position.at(2));
 		GetComponent<Text>(i).SetTextSize(TEXT_SIZE_DEFAULT);
 		GetComponent<Image>(i).SetPosition(POSITION_DEFAULT);
 		basePosList_.push_back(POSITION_DEFAULT);
@@ -160,6 +220,7 @@ void SelectUI::Initialize()
 	Selection_[0] = moveUIList_.begin();
 	Selection_[1] = Selection_[0] + 1;
 	Selection_[2] = Selection_[1] + 1;
+	Selection_[3] = Selection_[2] + 1;
 	buttonNum_ = *Selection_[0];
 	
 	GetComponent<Text>(buttonNum_).SetTextSize(TEXT_SIZE_PICKUP);
@@ -268,15 +329,16 @@ void SelectUI::PlayerCount(const int& padButton)
 void SelectUI::MoveButton(float ratio)
 {
 	float delta = MOVE * ratio * moveDir_;
+	int i = 0;
 	for(auto&& itr : Selection_)
 	{
 		GetComponent<Image>(*itr.second).
-			SetPosition({ -basePosList_[*itr.second].x + IMAGE_OFFSET.x,
+			SetPositionAtPixel({ -basePosList_[*itr.second].x + IMAGE_OFFSET.x,
 								 (basePosList_[*itr.second].y + IMAGE_OFFSET.y + delta) * IMAGE_RATIO,
 								  basePosList_[*itr.second].z });
 		GetComponent<Text>(*itr.second).
-			SetRatio( basePosList_[*itr.second].x,
-						  basePosList_[*itr.second].y + delta );
+			SetPosition({ basePosList_[*itr.second].x,
+						  basePosList_[*itr.second].y + delta });
 	}
 	//for (int& num : moveUIList_)
 	//{
@@ -312,6 +374,7 @@ void SelectUI::Release()
 
 void SelectUI::ItrIncrement()
 {
+	Selection_[3] = Selection_[2];
 	Selection_[2] = Selection_[1];
 	Selection_[1] = Selection_[0];
 	Selection_[0] = Selection_[-1];
@@ -329,20 +392,21 @@ void SelectUI::ItrDecrement()
 	Selection_[-1] = Selection_[0];
 	Selection_[0] = Selection_[1];
 	Selection_[1] = Selection_[2];
-	if (++Selection_[2] == end(moveUIList_))
-		Selection_[2] = moveUIList_.begin();
+	Selection_[2] = Selection_[3];
+	if (++Selection_[3] == end(moveUIList_))
+		Selection_[3] = moveUIList_.begin();
 
 	buttonNum_ = *Selection_[0];
 }
 
 void SelectUI::UIPositioning()
 {
-	for (int i = -2; i <= 2; i++)
+	for (int i = -2; i <= 3; i++)
 	{
-		GetComponent<Text>(*Selection_[i]).SetRatio(Position.at(i).x, Position.at(i).y);
+		GetComponent<Text>(*Selection_[i]).SetPosition(Position.at(i));
 		basePosList_[*Selection_[i]] = { Position.at(i).x, Position.at(i).y, 0 };
 		GetComponent<Image>(*Selection_[i]).
-			SetPosition({ -Position.at(i).x + IMAGE_OFFSET.x,
+			SetPositionAtPixel({ -Position.at(i).x + IMAGE_OFFSET.x,
 								 (Position.at(i).y + IMAGE_OFFSET.y) * IMAGE_RATIO, 0 });
 	}
 }
